8dfd0f137468e2e85e94de1775e482f3d1cb3308
William Han <willhan327@outlook.com>
2022-08-22 00:00:44 -0700

    update show_video  function

--- explore.py
+++ explore.py
@@ -4,9 +4,9 @@ import cv2
 import torch
 
 
-def show_labeled_video(video):
-    # --- shows labeled video
-
+def show_video(path_to_video):
+    # --- shows video
+    video = cv2.VideoCapture(path_to_video)
     while(video.isOpened()):
         ret, frame = video.read()
         if ret != False:
@@ -18,24 +18,10 @@ def show_labeled_video(video):
     video.release()
     cv2.destroyAllWindows()
 
-
-def show_unlabeled_video(video):
-    # --- shows unlabeled video
-
-    while(video.isOpened()):
-        ret, frame = video.read()
-        print(frame)
-        if ret != False:
-            cv2.imshow('frame', frame)
-        else:
-            break
-        if (cv2.waitKey(1) == ord('q')):
-            break
-    video.release()
-    cv2.destroyAllWindows()
 
 def videos_array(path_to_labeled_vides, path_to_unlabeled_videos):
     # --- returns 4d array of all videos from labeled and unlabeled
+    
 
 
 
@@ -44,13 +30,9 @@ def videos_array(path_to_labeled_vides, path_to_unlabeled_videos):
 
 if __name__ == '__main__':
     # --- example labeled video
-    # labeled_video = cv2.VideoCapture('labeled/0.hevc')
+    # labeled_video = 'labeled/0.hevc'
+    # show_video(labeled_video)
 
     # --- example unlabeled video
-    # unlabeled_video = cv2.VideoCapture('unlabeled/5.hevc')
-
-    # --- show labeled video
-    # show_labeled_video(labeled_video)
-
-    # --- show unlabeled video
-    # show_unlabeled_video(unlabeled_video)
+    # unlabeled_video = 'unlabeled/5.hevc'
+    # show_video(unlabeled_video)
