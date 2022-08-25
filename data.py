132aed604e48a0078873ade108c80f17ec3a51da
William Han <willhan327@outlook.com>
2022-08-25 00:51:47 -0700

    capture all frames // unsuccessful

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ from tkinter import N
 import numpy as np
 import pandas as pd
 import cv2
+from torch.utils.data import Dataset, DataLoader
 import torch
 import os
 
@@ -11,6 +12,8 @@ def show_video(path_to_video):
     video = cv2.VideoCapture(path_to_video)
     while(video.isOpened()):
         ret, frame = video.read()
+        # print(frame)
+        # break 
         if ret != False:
             cv2.imshow('frame', frame)
         else:
@@ -25,22 +28,30 @@ def videos_array(path_to_labeled_videos, path_to_unlabeled_videos):
     # --- returns 4d array of all videos from labeled and unlabeled
     labeled_list = []
     for file in sorted(os.listdir(path_to_labeled_videos)):
-        if '.hevc' in file:
-            labeled_video = cv2.VideoCapture(f'{path_to_labeled_videos}/{file}')
-            _, labeled_frame = labeled_video.read()
-            labeled_list.append(labeled_frame)
+        labeled_video = cv2.VideoCapture(f'{path_to_labeled_videos}/{file}')
+        while labeled_video.isOpened():
+            ret, labeled_frame = labeled_video.read()
+            if ret != False:
+                labeled_list.append(labeled_frame)
+            else:
+                break
+        labeled_video.release()
 
     unlabeled_list = []
     for file in sorted(os.listdir(path_to_unlabeled_videos)):
-        if '.hevc' in file:
-            unlabeled_video = cv2.VideoCapture(f'{path_to_unlabeled_videos}/{file}')
-            _, unlabeled_frame = unlabeled_video.read()
-            unlabeled_list.append(unlabeled_frame)
-
+        
+        unlabeled_video = cv2.VideoCapture(f'{path_to_unlabeled_videos}/{file}')
+        while unlabeled_video.isOpened(): 
+            ret, unlabeled_frame = unlabeled_video.read()
+            if ret != False:
+                unlabeled_list.append(unlabeled_frame)
+            else:
+                break
+        unlabeled_video.release()
     labeled_array = np.array(labeled_list)
-    # print('Shape of labeled arrays: ', labeled_array.shape)
+    print('Shape of labeled arrays: ', labeled_array.shape)
     unlabeled_array = np.array(unlabeled_list)
-    # print('Shape of unlabeled arrays: ', unlabeled_array.shape)
+    print('Shape of unlabeled arrays: ', unlabeled_array.shape)
     return labeled_array, unlabeled_array
 
 
@@ -63,12 +74,39 @@ def load_txt(path_to_txt):
     pitch_yaw = np.array(pitch_yaw, dtype = 'object')
     pitch = np.array(pitch, dtype = 'object')
     yaw = np.array(yaw, dtype = 'object')
-    print('Shape of pitch_yaw arrays: ', pitch_yaw.shape)
-    print('Shape of pitch arrays: ', pitch.shape)
-    print('Shape of yaw arrays: ', yaw.shape)
+    # print('Shape of pitch_yaw arrays: ', pitch_yaw.shape)
+    # print('Shape of pitch arrays: ', pitch.shape)
+    # print('Shape of yaw arrays: ', yaw.shape)
 
     return pitch_yaw, pitch, yaw
 
+
+class torch_dataset(Dataset):
+    def __init__(self, labeled_array, unlabeled_array, pitch_yaw, pitch, yaw):
+        self.labeled_array = labeled_array
+        self.unlabeled_array = unlabeled_array
+        self.pitch_yaw = pitch_yaw
+        self.pitch = pitch
+        self.yaw = yaw
+
+        @property
+        def n_insts(self):
+            return len(self.pitch_yaw)
+
+        @property
+        def labeled_len(self):
+            return self.labeled_array.shape
+        
+        def unlabeled_len(self):
+            return self.labeled_array.shape
+
+        def __len__(self):
+            return self.n_insts
+        
+        # def __getitem__(self, item):
+            # labeled = 
+
+        
 
 if __name__ == '__main__':
     # --- example labeled video
@@ -83,8 +121,10 @@ if __name__ == '__main__':
     labeled_array, unlabeled_array = videos_array('labeled', 'unlabeled')
 
     # --- load in pitch and yaw angles (radians) from txt files 
-    pitch_yaw, pitch, yaw = load_txt('labeled')
+    pitch_yaw, pitch, yaw = load_txt('labels')
     
+    # print(pitch_yaw[0].shape)
+    # print(labeled_array.shape)
 
 
 
